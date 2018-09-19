2b6fcabb63d350ec18d7318acf1682a97d8e3a98
Eric Chauvin <ericsourcecode@gmail.com>
2018-09-19 11:49:51 -0600

    Create InitialDefines.h

---
+++ InitialDefines.h
@@ -0,0 +1,11 @@
+// This tests what macros are defined before any
+// #include files are included.
+
+
+// This one is defined for my computer.
+// #ifdef __x86_64__
+// #error _x86_64 is defined.
+// #endif
+
+
+//
